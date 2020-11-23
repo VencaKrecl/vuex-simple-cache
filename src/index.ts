d7dd09a981f213f3f79f66bc41bda22009aeccdd
Venca Krecl <venca.email@gmail.com>
2020-11-23 10:03:06 -0800

    Fix code style

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,34 @@
-import { ActionContext } from 'vuex'
+import { ActionContext } from 'vuex';
 
-export const cacheAction = <S extends Record<string, any>, R>(key: string, action: (context: ActionContext<S, R>) => any): any => {
+export const cacheAction = <S extends Record<string, any>, R>(
+  key: string,
+  action: (context: ActionContext<S, R>) => any
+): any => {
   return (context: ActionContext<S, R>) => {
     if (!(key in context.state)) {
-      throw new Error(`Key "${key}" in state doesn't exist.`)
+      throw new Error(`Key "${key}" in state doesn't exist.`);
     }
 
     if (Array.isArray(context.state[key])) {
       if (context.state[key].length) {
-        return context.state[key]
+        return context.state[key];
       }
 
-      return action(context)
+      return action(context);
     }
 
     if (context.state[key] instanceof Object) {
       if (Object.keys(context.state[key]).length) {
-        return context.state[key]
+        return context.state[key];
       }
 
-      return action(context)
+      return action(context);
     }
 
     if (context.state[key]) {
-      return context.state[key]
+      return context.state[key];
     }
 
-    return action(context)
-  }
-}
+    return action(context);
+  };
+};
